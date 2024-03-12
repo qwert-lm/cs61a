34bf40884318cbf8f259a2fc1c08fb03b0879d6c
Liu Meng <871346477@qq.com>
2024-03-12 19:43:51 +0800

    finish hw07

--- hw07/hw07.scm
+++ hw07/hw07.scm
@@ -9,11 +9,24 @@
       #t
       (if (null? (cdr asc-lst))
           #t
-          (if (<= (car asc-lst)
-                  (cadr asc-lst))
-                  (ascending? (cdr asc-lst))
-                  #f))))
+          (if (<= (car asc-lst) (cadr asc-lst))
+              (ascending? (cdr asc-lst))
+              #f))))
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (if (= base 1)
+      1
+      (if 
+          (= exp 2)
+          (square base)
+          (if 
+            (even? exp)
+            (square (pow base (/ exp 2)))
+            (* base (pow base (- exp 1)))
+              )
+
+          )
+      )
+)
